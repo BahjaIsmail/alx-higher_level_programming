7154c5a9a0e90cca37e7a39f6a90420172af29f7
BahjaIsmail <Bahjaismail09@gmail.com>
2024-03-16 06:15:02 -0700

    seocnd

--- 0x01-python-if_else_loops_functions/2-print_alphabet.py
+++ 0x01-python-if_else_loops_functions/2-print_alphabet.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
-for a in range(97, 124):
-    if a != 101 and a != 113:
-        print("{}".format(chr(a)), end="")
+for a in range(97, 123):
+    print(chr(a), end='')
+
+print()
