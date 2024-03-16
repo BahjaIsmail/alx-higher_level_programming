a0dc22b72a26f1795c9890904d4c8c05a93ffff7
BahjaIsmail <Bahjaismail09@gmail.com>
2024-03-16 13:05:50 -0700

    all

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -9,5 +9,5 @@ if __name__ == "__main__":
        print("1 arguments:")
     else:
        print("{} arguments:".format(count))
-    for a in range(count):
-       print("{}: {}".format(a + 1, sys.argv[a + 1]))
+    for i in range(count):
+       print("{}: {}".format(i + 1, sys.argv[i + 1]))
