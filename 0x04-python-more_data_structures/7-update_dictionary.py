ba0093e0c4e9462aa684da06acd9a9ceb59ab76b
BahjaIsmail <Bahjaismail09@gmail.com>
2024-03-18 02:49:23 -0700

    all

--- 0x04-python-more_data_structures/7-update_dictionary.py
+++ 0x04-python-more_data_structures/7-update_dictionary.py
@@ -1,3 +1,6 @@
 #!/usr/bin/python3
 def update_dictionary(a_dictionary, key, value):
-    a_dictionary[key] = value
+    if k in a_dictionary:
+        a_dictionary[k] = v
+    else:
+        a_dictionary.update({k:v})
