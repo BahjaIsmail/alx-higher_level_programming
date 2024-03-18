6f66a1e03d69d58daca868d5f5b269fec4fb2ca5
BahjaIsmail <Bahjaismail09@gmail.com>
2024-03-18 02:09:02 -0700

    all

--- 0x04-python-more_data_structures/2-uniq_add.py
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 def uniq_add(my_list=[]):
-    converted_list_to_set  = set(my_list)
-    sum_of_new_set = sum(converted_list_to_set)
+    converted_set  = set(my_list)
+    sum_of_new_set = sum(converted_set)
     return sum_of_new_set
