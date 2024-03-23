a734be88fc27f0a1d370ad7c728aedeca8070de7
BahjaIsmail <Bahjaismail09@gmail.com>
2024-03-23 06:50:38 -0700

    sql

--- 0x0D-SQL_introduction/11-best_score.sql
+++ 0x0D-SQL_introduction/11-best_score.sql
@@ -1,2 +1,2 @@
 -- selecting data from second table with score greater than or equalt to 10
-SELECT score, name FROM second_table where score >= 10
+SELECT score, name FROM second_table WHERE score >= 10 ORDER BY score DESC;
