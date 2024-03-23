13271c48d47a4907858b89f69f2740e624649f68
BahjaIsmail <Bahjaismail09@gmail.com>
2024-03-23 06:45:21 -0700

    sql

--- 0x0D-SQL_introduction/8-count_89.sql
+++ 0x0D-SQL_introduction/8-count_89.sql
@@ -1,2 +1,2 @@
--- Total records with id=89
+-- Retrieve the count of records in the "first_table" of the "hbtn_0c_0" database on your MySQL server where the id is equal to 89.
 select count(*) from first_table where id =89
