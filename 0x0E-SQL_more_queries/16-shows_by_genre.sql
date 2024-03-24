4c4734006c27deaf39d1c7f1240d493882175a29
BahjaIsmail <Bahjaismail09@gmail.com>
2024-03-24 10:57:52 -0700

    sql

--- 0x0E-SQL_more_queries/16-shows_by_genre.sql
+++ 0x0E-SQL_more_queries/16-shows_by_genre.sql
@@ -1,9 +1,8 @@
 -- listing all shows, and all genres linked to that show with or without Genre using multiple left joins
-SELECT tv_shows.title,tv_genres.name AS genre
+SELECT tv_shows.title, tv_genres.name
 FROM tv_shows
-LEFT JOIN tv_show_genres 
+LEFT JOIN tv_show_genres
 ON tv_shows.id = tv_show_genres.show_id
-LEFT JOIN tv_genres 
+LEFT JOIN tv_genres
 ON tv_show_genres.genre_id = tv_genres.id
-ORDER BY tv_shows.title ASC,tv_genres.name ASC;
-
+ORDER BY tv_shows.title, tv_genres.name ASC;
