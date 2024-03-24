194cb74e89ea5363dcb782eee0a13432366eafea
BahjaIsmail <Bahjaismail09@gmail.com>
2024-03-24 10:46:01 -0700

    sql

--- 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
+++ 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
@@ -1,4 +1,5 @@
 -- listing all the cities of california using NESTED Query
-SELECT * FROM cities
+SELECT id, name
+FROM cities
 WHERE state_id = (SELECT id FROM states WHERE name = 'California') ORDER BY id ASC;
 
